e48f150aee72d5d2146c8fc69446a0227c5918ba
Roubak <admin@wl4s-248-227.unil.ch>
2017-06-13 14:59:24 +0200

    starting leaflet project

--- index.js
+++ index.js
@@ -1,76 +1,23 @@
-// var map = new google.maps.Map(d3.select("#map").node(), {
-//   zoom: 8,
-//   center: new google.maps.LatLng(37.76487, -122.41948),
-//   mapTypeId: google.maps.MapTypeId.TERRAIN
-// });
-//
-// // Load the station data. When the data comes back, create an overlay.
-// d3.json("stations.json", function(error, data) {
-//   if (error) throw error;
-//
-//   var overlay = new google.maps.OverlayView();
-//
-//   // Add the container when the overlay is added to the map.
-//   overlay.onAdd = function() {
-//     var layer = d3.select(this.getPanes().overlayLayer).append("div")
-//         .attr("class", "stations");
-//
-//     // Draw each marker as a separate SVG element.
-//     // We could use a single SVG, but what size would it have?
-//     overlay.draw = function() {
-//       var projection = this.getProjection(),
-//           padding = 10;
-//
-//       var marker = layer.selectAll("svg")
-//           .data(d3.entries(data))
-//           .each(transform) // update existing markers
-//         .enter().append("svg")
-//           .each(transform)
-//           .attr("class", "marker");
-//
-//       // Add a circle.
-//       marker.append("circle")
-//           .attr("r", 4.5)
-//           .attr("cx", padding)
-//           .attr("cy", padding);
-//
-//       // Add a label.
-//       marker.append("text")
-//           .attr("x", padding + 7)
-//           .attr("y", padding)
-//           .attr("dy", ".31em")
-//           .text(function(d) { return d.key; });
-//
-//       function transform(d) {
-//         d = new google.maps.LatLng(d.value[1], d.value[0]);
-//         d = projection.fromLatLngToDivPixel(d);
-//         return d3.select(this)
-//             .style("left", (d.x - padding) + "px")
-//             .style("top", (d.y - padding) + "px");
-//       }
-//     };
-//   };
-//
-//   // Bind our overlay to the map…
-//   overlay.setMap(map);
-// });
+var map = new L.Map("map", {center: [37.8, -96.9], zoom: 4})
+    .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
 
+var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+    g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
+d3.json("data/hectpopVD.geojson", function(error, collection) {
+  if (error) throw error;
 
-var map = new google.maps.Map(d3.select("#map").node(), {
-  zoom: 8,
-  center: new google.maps.LatLng(37.76487, -122.41948),
-  mapTypeId: google.maps.MapTypeId.TERRAIN
+  function projectPoint(x, y) {
+    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
+  }
+
+  var transform = d3.geo.transform({point: projectPoint}),
+    path = d3.geo.path().projection(transform);
+
+  var feature = g.selectAll("path")
+    .data(collection.features)
+    .enter().append("path");
+
+  
 });
-
-var overlay = new google.maps.OverlayView();
-
-function transform(d) {
-  d = new google.maps.LatLng(d.value[1], d.value[0]);
-  d = projection.fromLatLngToDivPixel(d);
-  return d3.select(this)
-      .style("left", (d.x - padding) + "px")
-      .style("top", (d.y - padding) + "px");
-}
-
-overlay.setMap(map);
