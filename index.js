81b1230ee831c9b887e051e688c0cf4cec76f706
nvallott <nvallott@hotmail.com>
2017-06-15 13:42:05 +0200

    change coordinates, basemap, new layer for pts

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var map = new L.Map("map", {center: [37.8, -96.9], zoom: 4})
+var map = new L.Map("map", {center: [46.53, -6.62], zoom: 9})
     .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
 
 var svg = d3.select(map.getPanes().overlayPane).append("svg"),
@@ -12,12 +12,12 @@ d3.json("data/hectpopVD.geojson", function(error, collection) {
     this.stream.point(point.x, point.y);
   }
 
-  var transform = d3.geo.transform({point: projectPoint}),
-    path = d3.geo.path().projection(transform);
+  var transform = d3.geoTransform({point: projectPoint}),
+    path = d3.geo.projection(transform);
 
   var feature = g.selectAll("path")
     .data(collection.features)
     .enter().append("path");
 
-  
+
 });
