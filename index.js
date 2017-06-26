438f7d6c0c8ca94a2b827bcdda087838d61e6795
roubaka <lucas.martinez787@gmail.com>
2017-06-26 18:54:13 +0200

    working on heatmap opacity

--- index.js
+++ index.js
@@ -1,79 +1,122 @@
+O = {};
 
-// Initiaize the map
-var map = new L.map("map", {center: [46.52, 6.60], zoom: 14});
-var cartodb = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
-});
+// Methods to get the size of the browser viewport
+let windowHeight = $(window).height();  // returns height of browser viewport
+let windowWidth = $(window).width();  // returns width of browser viewport
 
-// Add the cartodb layer to the map
-cartodb.addTo(map);
+// GLOBAL VARIABLES
+let map;
+// let heatmap;
 
-// // Creating the heatmap based on the poputlation hectares
-// let heathect = [];
-// // Loading population datas
-// d3.csv("data/hectpop_xy4.csv", function(data) {
-//  	for(i = 0; i < data.length ; i++){
-// 		heathect.push([]);
-// 		heathect[i][0] = data[i].Y;
-// 		heathect[i][1] = data[i].X;
-// 		heathect[i][2] = data[i].B14BTOT;
-// 	}
-//
-// // Adding the heatmap layer to the map
-// 	let heatmap = L.heatLayer(heathect, {radius: 25, max:65}).addTo(map);
-//  })
+O.main = function(){
+	O.initMap();
+};
 
-// Creating the public transportation layer
-let ptStops = [];
-
-let ptsOverlay = L.d3SvgOverlay(function(sel,proj){
-	var ptsUpd = sel.selectAll('circle').data(ptStops);
-	ptsUpd.enter()
-				.append("circle")
-				.attr('cx', function(d){return proj.latLngToLayerPoint(d.latLng).x;})
-				.attr('cy', function(d){return proj.latLngToLayerPoint(d.latLng).y;})
-				.attr('r', 6)
-				.attr('fill', function(d){
-					if(d.MOYEN_TRAN.match('CheminFer')){
-						return "blue"
-					} else if(d.MOYEN_TRAN == 'Bus'){
-						return "lime"
-					} else {
-						return "turquoise"
-					}
-				})
-				.attr('opacity', 0.5)
-				.attr('class', function(d){
-					if(d.MOYEN_TRAN.match('CheminFer')){
-						return "bigBuff"
-					} else if(d.MOYEN_TRAN == 'Bus'){
-						return "smallBuff"
-					} else {
-						return "midBuff"
-					}
-				})
+O.initMap = function(){
+	// Initiaize the map
+	map = new L.map("map", {center: [46.52, 6.60], zoom: 14});
+	let cartodb = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
 	});
 
-	tooltip = d3.select('#map')
-							.append('div')
-							.attr('class','tooltip');
+	// Add the cartodb layer to the map
+	cartodb.addTo(map);
 
+	O.makeHeatMap();
+	setTimeout(function(){
+		O.makePtStops();
+	}, 250);
 
-d3.selectAll('circle')
-	.on('mouseover', function(d){
-		d3.select(this)
-		.attr('pointer-events','visible')
+	// var opacitySlider = new L.Control.opacitySlider();
+	// map.addControl(opacitySlider);
+	// opacitySlider.setOpacityLayer(heatmap);
+};
+
+O.makeHeatMap = function(){
+	// Creating the heatmap based on the poputlation hectares
+	let heathect = [];
+	// Loading population datas
+	d3.csv("data/hectpop_xy4.csv", function(data) {
+		for(i = 0; i < data.length ; i++){
+			heathect.push([]);
+			heathect[i][0] = data[i].Y;
+			heathect[i][1] = data[i].X;
+			heathect[i][2] = data[i].B14BTOT;
+		}
+
+		// Adding the heatmap layer to the map
+		let heatmap = L.heatLayer(heathect, {radius: 25, max:65}).addTo(map);
+		// heatmap.setZIndex(0);
+	})
+	console.log("1");
+	// O.makePtStops();
+};
+
+O.makePtStops = function(){
+	// Creating the public transportation layer
+	let ptStops = [];
+
+	let ptsOverlay = L.d3SvgOverlay(function(sel,proj){
+		var ptsUpd = sel.selectAll('circle').data(ptStops);
+		ptsUpd.enter()
+		.append("circle")
+		.attr('cx', function(d){return proj.latLngToLayerPoint(d.latLng).x;})
+		.attr('cy', function(d){return proj.latLngToLayerPoint(d.latLng).y;})
+		.attr('r', 6)
+		.attr('fill', function(d){
+			if(d.MOYEN_TRAN.match('CheminFer')){
+				return "blue"
+			} else if(d.MOYEN_TRAN == 'Bus'){
+				return "lime"
+			} else {
+				return "turquoise"
+			}
+		})
+		.style('stroke','black')
+		.attr('opacity', 0.5)
+		.attr('class', function(d){
+			if(d.MOYEN_TRAN.match('CheminFer')){
+				return "bigBuff"
+			} else if(d.MOYEN_TRAN == 'Bus'){
+				return "smallBuff"
+			} else {
+				return "midBuff"
+			}
+		});
+	});
+
+	// ptsOverlay.setZIndex(15);
+	// ptsOverlay.bringToFront()
+
+	// Loading the public transportation datas
+	d3.csv("data/pts_buff.csv",function(data){
+		ptStops = data.map(function(d){
+			d.latLng = [+d.Y,+d.X];
+			// d.ALTITUDE = (d.ALTITUDE == '') ? 50 : +d.ALTITUDE; //NAs
+			return d;
+		});
+		ptsOverlay.addTo(map);
+	});
+
+	setTimeout(function(){
+		d3.selectAll('circle').on('mouseover',function(){
+			d3.select(this)
 			.transition()
-			.duration(100)
-			.attr('r', 15);
-	});
+			.duration(50)
+			.attr('r', function(){
+				return $('#test').val();
+			});
+		});
+		d3.selectAll('circle').on('mouseout', function(){
+			d3.select(this)
+			.transition()
+			.duration(200)
+			.attr('r', 6);
+		});
+	}, 1000);
+	console.log("2");
+};
 
-// Loading the public transportation datas
-d3.csv("data/pts_buff.csv",function(data){
-ptStops = data.map(function(d){
-  d.latLng = [+d.Y,+d.X];
-  // d.ALTITUDE = (d.ALTITUDE == '') ? 50 : +d.ALTITUDE; //NAs
-  return d;
-});
-ptsOverlay.addTo(map);
-});
+O.changeOpacity = function(){
+	d3.selectAll('.leaflet-heatmap-layer').style('opacity',0.5);
+};
