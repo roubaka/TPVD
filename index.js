ee106da973e1e8ab776c876bbb216996de6b9926
roubaka <lucas.martinez787@gmail.com>
2017-07-18 20:56:06 +0200

    commenting code - html ok

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-O = {};
+// TPVD - Nicolas Vallotton & Lucas Martinez - Juillet 2017
 
-// Methods to get the size of the browser viewport
+// Creating APP object for storing all Methods
+APP = {};
+
+/*****
+Declaring global variables
+*****/
+
+// Storing size of the browser viewport
 let windowHeight = $(window).height();  // returns height of browser viewport
 let windowWidth = $(window).width();  // returns width of browser viewport
 
+
+// Map
+let map;
+// Tooltip of the map
+let tooltipMap;
+
+// Array of 0 to initialize correct number of dots for scatterplot
 let dataGraph = [0,0,0,0,0,0,0,0,0,0];
-let line;
-
-let gradient = ['red', 'yellow', 'lime', 'cyan', 'blue'];
-
-// Dictionnary of buffer size and values linked to slider
-let bufferVal = [];
-
+// Boolean to check if graph has already been initialized
 let graphInitialized = false;
 
+// Correspondance table for slider values, buffer label in meters, buffer sizes in pixel and pop values for each
+let bufferVal = []; // initialized empty
 for(i = 1; i <= 10; i++){
     bufferVal.push({"sliderVal": i, "buffer": `${i*100}m`, "bufferPx": i*15, "pop":`pop${i*100}m`});
 }
 
-// GLOBAL VARIABLES
-let map;
-let tooltipMap;
-
-// Initiaize the whole script of the page
-O.main = function(){
-    O.initMap();
-    O.sliderevent();
+/*****
+Initializing the whole script of the page
+*****/
+APP.main = function(){
+    APP.initMap();
+    APP.sliderevent();
 };
 
-O.initMap = function(){
-    // Initiaize the map
+/*****
+Initializing map - leaflet with cartodb basemap and tooltip ready
+*****/
+APP.initMap = function(){
+    // Initiaize the map - definig parameters and adding cartodb basemap
     map = new L.map("map", {center: [46.515228504114376, 6.629819869995117], zoom: 14, minZoom: 10, maxZoom: 15, maxBounds: ([[46.128688, 5.971754],[47.121474, 7.313116]])});
     let cartodb = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy;<a href="https://carto.com/attribution">CARTO</a>'
@@ -38,25 +49,28 @@ O.initMap = function(){
     // Add the cartodb layer to the map
     cartodb.addTo(map);
 
-    O.makeHeatMap();
+    // Calling metho to create heatmap overlay
+    APP.makeHeatMap();
+    // Calling method to create - has to wait for the map to be created
     setTimeout(function(){
-        O.makePtStops();
+        APP.makePtStops();
     }, 250);
 
-    // var opacitySlider = new L.Control.opacitySlider();
-    // map.addControl(opacitySlider);
-    // opacitySlider.setOpacityLayer(heatmap);
-
+    // Getting tooltip ready for showing data
     tooltipMap = d3.select('#map')
     .append('div')
     .attr('class', 'tooltip');
 };
 
-O.makeHeatMap = function(){
-    // Creating the heatmap based on the poputlation hectares
+/*****
+Creating heatmap overlay based on the populated hectares
+*****/
+APP.makeHeatMap = function(){
+    // Empty array
     let heathect = [];
     // Loading population datas
     d3.csv("data/hectpop_xy4.csv", function(data) {
+        // Formatting data for heatLayer function
         for(i = 0; i < data.length ; i++){
             heathect.push([]);
             heathect[i][0] = data[i].Y;
@@ -64,57 +78,70 @@ O.makeHeatMap = function(){
             heathect[i][2] = data[i].B14BTOT;
         }
 
-        // Adding the heatmap layer to the map
+        // Adding the heatmap layer to the map - radius and max parameters optimized to get readable map through whole zoom range
         let heatmap = L.heatLayer(heathect, {radius: 30, max: 500}).addTo(map);
-        O.changeOpacity();
+        // Calling method to opacity of heatmap
+        APP.changeOpacity();
     })
 
-    // Adding Legend
+    // Declaring legend to be place over the map
     var legend = L.control({position: 'bottomright'});
 
     legend.onAdd = function (map) {
         // Creating div for legend
         let div = L.DomUtil.create('div', 'info legend');
 
-        // Inserting HTML table for gradient color and labels
+        // Inserting HTML table for custom gradient color and labels
         div.innerHTML = 'POPULATION <table> <tbody> <tr> <td> <canvas id="myCanvas" width="20" height="75" style="border:1px solid #d3d3d3;opacity:0.7"> </canvas> </td> <td> &nbsp; Elevée </br> </br> </br> </br> &nbsp; Faible </td> </tr> </tbody> </table>';
         return div;
     };
 
+    // Adding legend to the map
     legend.addTo(map);
 
-    O.colorLegend();
-    // O.makePtStops();
+    // Calling method for coloring legend
+    APP.colorLegend();
 };
 
-O.colorLegend = function(){
+/*****
+Coloring HTML part for the legend
+*****/
+APP.colorLegend = function(){
     // Adding color gradient to the legend
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     var grd = ctx.createLinearGradient(0,0,0,75);
 
+    // Creating different stops and color gradient
     grd.addColorStop(0, "red");
     grd.addColorStop(0.25, "yellow");
     grd.addColorStop(0.5, "lime");
     grd.addColorStop(0.75, "cyan");
     grd.addColorStop(1, "blue");
 
+    // Parameters for HTML color gradient
     ctx.fillStyle = grd;
     ctx.fillRect(0, 0, 256, 256);
 }
 
-O.makePtStops = function(){
-    // Creating the public transportation layer
+/*****
+Creating points for PT stops and adding them to the map
+*****/
+APP.makePtStops = function(){
+    // Empty array to store
     let ptStops = [];
 
+    // Creating the public transportation layer with leaflet d3 svg overlay according to correct projection
     let ptsOverlay = L.d3SvgOverlay(function(sel,proj){
+
         var ptsUpd = sel.selectAll('circle').data(ptStops);
         ptsUpd.enter()
         .append("circle")
-        .attr('cx', function(d){return proj.latLngToLayerPoint(d.latLng).x;})
-        .attr('cy', function(d){return proj.latLngToLayerPoint(d.latLng).y;})
+        .attr('cx', function(d){return proj.latLngToLayerPoint(d.latLng).x;}) // projecting points
+        .attr('cy', function(d){return proj.latLngToLayerPoint(d.latLng).y;}) // projecting points
         .attr('r', 6)
         .attr('fill', function(d){
+            // different fill color according to transport type
             if(d.MOYEN_TRAN.match('CheminFer')){
                 return "blue"
             } else if(d.MOYEN_TRAN == 'Bus'){
@@ -125,9 +152,9 @@ O.makePtStops = function(){
         })
         .style('position', 'relative')
         .style('stroke','black')
-        // .style('z-index', 10000000)
         .attr('opacity', 0.5)
         .attr('class', function(d){
+            // different class attribute according to transport type
             if(d.MOYEN_TRAN.match('CheminFer')){
                 return "bigBuff dot"
             } else if(d.MOYEN_TRAN == 'Bus'){
@@ -138,27 +165,28 @@ O.makePtStops = function(){
         });
     });
 
-    // ptsOverlay.setZIndex(15);
-    // ptsOverlay.bringToFront()
-
     // Loading the public transportation datas
     d3.csv("data/buffers.csv",function(data){
-        console.log(data);
+        // mapping data to get proper latLong values
         ptStops = data.map(function(d){
             d.latLng = [+d.Y,+d.X];
             return d;
         });
+        // Adding layer to the map
         ptsOverlay.addTo(map);
+
     });
 
+    // Defining interaction events - has to wait for the data to be loaded
     setTimeout(function(){
         d3.selectAll('.dot')
+        // Changing buffer size according to selected values on mouseover + tooltip infos
         .on('mouseover',function(d){
             d3.select(this)
-            // .style('z-index', 10)
             .transition()
             .duration(100)
             .attr('r', function(d){
+                // For each type of buffer, get the pixel size in the correspondance table bufferVal
                 if(d.MOYEN_TRAN.match('CheminFer')){
                     return bufferVal[$('#slider1').val()-1].bufferPx;
                 } else if(d.MOYEN_TRAN == 'Bus'){
@@ -167,7 +195,9 @@ O.makePtStops = function(){
                     return bufferVal[$('#slider2').val()-1].bufferPx;
                 }
             });
+            // Showing value of buffer in the tooltip
             tooltipMap.html(function(){
+                // For each type of buffer, get the population value in the correspondance table bufferVal
                 let pop = "";
                 if(d.MOYEN_TRAN.match('CheminFer')){
                     pop = d[bufferVal[$('#slider1').val()-1].pop];
@@ -176,9 +206,11 @@ O.makePtStops = function(){
                 } else {
                     pop = d[bufferVal[$('#slider2').val()-1].pop];
                 }
+                // Replace unknown values with 0
                 if(pop == "NA"){
                     pop = 0;
                 }
+                // Return actual innerHTML text
                 return `${d.NOM} </br>
                 Population desservie : ${pop}`;
             })
@@ -187,8 +219,8 @@ O.makePtStops = function(){
             .style('opacity', 0.8)
             .style('left', `${d3.event.pageX}px`)
             .style('top', `${d3.event.pageY}px`);
-
         })
+        // Change html header for the graphic when buffer is clicked
         .on('click', function(d){
             $('#graphLegend').html(function(){
                 return `<table width="100%">
@@ -198,32 +230,38 @@ O.makePtStops = function(){
                 <tr> <td> Commune : ${d.NOM_COMMUN} </td> <td> &nbsp; </td> <td> Altitude : ${d.ALTITUDE} </td> </tr>
                 </table>`;
             })
+            // If the graph has been launched once, update it - Else, initialize it
             if(graphInitialized){
-                O.updateGraph(d);
+                APP.updateGraph(d);
             } else {
                 graphInitialized = true;
-                O.initGraph(d);
+                APP.initGraph(d);
             }
         })
+        // Reset normal size on buffer point on mouseout
         .on('mouseout', function(){
             d3.select(this)
-            // .style('z-index', 10000000)
             .transition()
             .duration(200)
             .attr('r', 6);
             tooltipMap.transition()
             .duration(200)
             .style('opacity', 0);
-
         });
     }, 1000);
 };
 
-O.changeOpacity = function(){
+/*****
+Changing heatmap opacity for better readability
+*****/
+APP.changeOpacity = function(){
     d3.selectAll('.leaflet-heatmap-layer').style('opacity',0.4);
 };
 
-O.sliderevent = function(){
+/*****
+Updating innerHTML of buffer size values according to slider value using conversion table
+*****/
+APP.sliderevent = function(){
     $('.slidBuffer').change(function(){
         $('#slider1_val').html(bufferVal[$('#slider1').val()-1].buffer);
         $('#slider2_val').html(bufferVal[$('#slider2').val()-1].buffer);
@@ -231,8 +269,13 @@ O.sliderevent = function(){
     });
 }
 
-O.initGraph = function(data){
+/*****
+
+*****/
+APP.initGraph = function(data){
+    // Removing introduction text for graph
     $('#tuto').remove();
+
     // Creating margins for the svg
     margin = {top : 40, right : 40, bottom : 55, left : 52};
 
@@ -292,30 +335,32 @@ O.initGraph = function(data){
     svgGraph.append('path')
     .attr('class','line')
     .style('stroke','none');
-    O.updateGraph(data);
+    APP.updateGraph(data);
 }
 
-O.updateGraph = function(data) {
+/*****
 
-    // Transforming data
+*****/
+APP.updateGraph = function(data) {
+
+    // Reset datagraph as empty array
     dataGraph = [];
+    // Formatting data to be used on scatterplot
     for(i = 1; i <= 10; i++){
         string = `pop${i*100}m`;
         dataGraph.push({"size": i*100, "pop": +data[string]});
     }
 
-    // Replacing all falsey values (espacially NaN) with 0
+    // Replacing falsey values (here NaN) with 0
     dataGraph.forEach(function(a){
         a.pop = a.pop || 0;
     })
 
-    // Setting up X
+    // Setting up X scale and axis
     xScale = d3.scale.linear().range([0,wGraph]).domain([0,1000]);
-    // xScale = d3.scale.linear().range([0,wGraph]);
-
     xAxis = d3.svg.axis().scale(xScale).orient('bottom');
 
-    // Setting up Y
+    // Setting up Y scale and axis
     let yMin = d3.min(dataGraph, function(d){return d.pop});
     let yMax = d3.max(dataGraph, function(d){return d.pop})
     yScale = d3.scale.linear().range([hGraph,0]).domain([yMin,yMax]);
